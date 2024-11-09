2b289c17ad78b53e58e2fcbbb5817a38af0c3630
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-11-09 17:57:16 +0530

    switched over to using local generated stubs for project-planton apis

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	gcpdnszonev1 "buf.build/gen/go/project-planton/apis/protocolbuffers/go/project/planton/provider/gcp/gcpdnszone/v1"
 	"github.com/pkg/errors"
 	"github.com/project-planton/gcp-dns-zone-pulumi-module/pkg"
-	"github.com/project-planton/pulumi-module-golang-commons/pkg/stackinput"
+	gcpdnszonev1 "github.com/project-planton/project-planton/apis/go/project/planton/provider/gcp/gcpdnszone/v1"
+	"github.com/project-planton/project-planton/pkg/pulmod/stackinput"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
